99de9d734fb1792f5bab2b322707129b983c413c
michael njenga <mickynduch@gmail.com>
2022-03-06 23:38:53 +0300

    js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function calculate_age(dob) {
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms);
+
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+console.log(calculate_age(new Date(1991, 11, 29)));
