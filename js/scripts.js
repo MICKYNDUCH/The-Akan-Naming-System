f4b003d445d8f1de3324fbcd32ba9d5a5f5d2b72
michael njenga <mickynduch@gmail.com>
2022-03-07 08:43:02 +0300

    add js script

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
+//
+// var days = {"sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
+// var maleNames = {"kwasi","kwando","kwabena","kwaku","yaw","kofi","kwame"};
+// var femaleNames ={"akosua","adwoa","abena","akua","yua","afua","ama"};
+// var weekNumder = {"0","1","2","3","4","5","6","7"};
+//
+// day===maleNames===femaleNames===weekNumder;
+
+
+
+// function calculate_age(dob) {
+//     var diff_ms = Date.now() - dob.getTime();
+//     var age_dt = new Date(diff_ms);
+//
+//     return Math.abs(age_dt.getUTCFullYear() - 1970);
+// }
+//
+// console.log(calculate_age(new Date(1991, 11, 29)));
+
 function calculate_age(dob) {
-    var diff_ms = Date.now() - dob.getTime();
-    var age_dt = new Date(diff_ms);
+  var diff_ms = Date.now() - dob.getTime();
+  var age_dt = new Date(diff_ms);
 
-    return Math.abs(age_dt.getUTCFullYear() - 1970);
+  return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
-
-console.log(calculate_age(new Date(1991, 11, 29)));
+var input = prompt("please input date = (yyyy mm dd)")
+console.log(calculate_age(new Date(input)));
